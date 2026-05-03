acb7cf5dcd7c86df5ecf6b8217ebfd182c4d9164
Riley <riley.childs@yahoo.com>
2026-05-03 12:36:40 -0400

    Added mobile responsive layout

--- wp-content/themes/lead-capture-theme/functions.php
+++ wp-content/themes/lead-capture-theme/functions.php
@@ -89,6 +89,8 @@ function lead_capture_theme_scripts() {
 	wp_enqueue_style( 'lead-capture-theme-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'lead-capture-theme-style', 'rtl', 'replace' );
 
+	wp_enqueue_script( 'lead-capture-theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
